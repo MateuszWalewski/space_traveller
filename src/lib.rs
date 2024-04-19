0cda29630c6dbe838a2fe4ac5deb3ba1055a9ef7
Mateusz Walewski <hraboa77@gmail.com>
2024-04-19 11:40:17 +0200

    extending the program's modularity...

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,49 @@
+use std::io;
 mod custom_iteration;
 
 pub use crate::custom_iteration::CycleIter;
 
+struct Player {
+    name: String,
+    score: u32,
+}
 
+pub struct GameController {
+    players: Vec<Player>,
+}
+
+impl GameController {
+    pub fn new() -> Self {
+        GameController {
+            players: Vec::new(),
+        }
+    }
+
+    pub fn add_players(&mut self) {
+        while self.players.len() < 2 {
+            println!("Please enter player nick:");
+            let mut nick = String::new();
+            io::stdin()
+                .read_line(&mut nick)
+                .expect("Failed to read nick");
+            self.players.push(Player {
+                name: nick,
+                score: 0,
+            });
+        }
+        println!("Players added! You can start the game!");
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn addition_of_players_works() {
+        let gc = GameController::new();
+
+
+        assert_eq!(4, 4);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,5 @@
-use std::io;
-use space_traveller::CycleIter;
+use space_traveller::{CycleIter, GameController};
 
-struct Player {
-    name: String,
-    score: u32,
-}
-
-struct GameController {
-    players: Vec<Player>,
-}
-
-impl GameController {
-    pub fn new() -> Self {
-        GameController {
-            players: Vec::new(),
-        }
-    }
-
-    pub fn add_players(&mut self) {
-        while self.players.len() < 2 {
-            println!("Please enter player nick:");
-            let mut nick = String::new();
-            io::stdin()
-                .read_line(&mut nick)
-                .expect("Failed to read nick");
-            self.players.push(Player {
-                name: nick,
-                score: 0,
-            });
-        }
-        println!("Players added! You can start the game!");
-    }
-}
 
 fn main() {
     let mut gc = GameController::new();
