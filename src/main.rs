cabb41013cd6bb7d1603201b6734314b4361e99d
Mateusz Walewski <hraboa77@gmail.com>
2024-04-19 10:18:26 +0200

    separating binary crate from the lib one

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,8 @@
-
-pub struct CirculatingIterator<'a, T> {
-    container: &'a Vec<T>,
-    index: usize
-}
-
-impl<'a,T> Iterator for CirculatingIterator<'a,T> {
-    type Item = &'a T;
-    
-    fn next(&mut self) -> Option<Self::Item> {
-        self.index = (self.index + 1) % self.container.len();
-        if self.container.len() != 0 {
-           Some(&self.container[self.index])
-        } else {
-            None
-        }
-    }
-}
-
-trait CircleIter<T> {
-    fn circle_iter(&self) -> CirculatingIterator<T>;
-}
-
-
-impl<T> CircleIter<T> for Vec<T> {
-    fn circle_iter(&self) -> CirculatingIterator<T> {
-        CirculatingIterator {
-            container: &self,
-            index: 0
-        }
-
-    }
-}
-
+use space_traveller::CircleIter;
 
 fn main() {
-    let container = vec![1,4,5,7];
-
-    for i in container.circle_iter().take(7){
+    let container = vec![1, 4, 5, 7];
+    for i in container.circle_iter().take(9) {
         println!("item: {}", i);
     }
-
-
 }
